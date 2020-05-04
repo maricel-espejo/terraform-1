4f807de0bbcc8f56d2bd2d0ef0501721576aed02
Jonathan Espejo <57280516+jonathan-espejo@users.noreply.github.com>
2020-05-04 15:43:40 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ cd $(dirname $0)
 rm -rf .terraform/environment
 terraform init -input=false
 terraform workspace select rds-dev || terraform workspace new rds-dev
-terraform plan -input=false
+terraform apply -input=false
